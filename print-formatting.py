c24c30c25d6da5b63fa2b27ffdb2c85780a9d3f7
sachinsuresh <sachinsuresh00@gmail.com>
2019-09-18 09:46:21 +0530

    Printed my details using print formatting

---
+++ print-formatting.py
@@ -0,0 +1,15 @@
+name = "Sachin"
+age = 23
+salary = 256843.56
+hair_colour = 'black'
+height = 185.1
+weight = 90.00
+education_degree = 'BE Mechanical Engineering'
+
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"I have a monthly salary of Rs {salary}")
+print(f"My hair is {hair_colour} in colour")
+print(f"I am {height} tall")
+print(f"I weigh {weight} kgs")
+print(f"I have a {education_degree} degree")
