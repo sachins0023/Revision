e1ae5a599558b237a4ccef577d579710f1983a2e
sachinsuresh <sachinsuresh00@gmail.com>
2019-09-18 11:12:02 +0530

    Calculator made

---
+++ calculator.py
@@ -0,0 +1,18 @@
+def add(num1,num2):
+    return num1+num2
+
+def subtract(num1,num2):
+    return num1-num2
+
+def multiply(num1,num2):
+    return num1*num2
+
+def divide(num1,num2):
+    return num1/num2
+
+first_number = 10
+second_number =2
+print(f"Sum of two numbers {first_number} and {second_number} is", add(first_number,second_number))
+print(f"Difference of two numbers {first_number} and {second_number} is", subtract(first_number,second_number))
+print(f"Product of two numbers {first_number} and {second_number} is", multiply(first_number,second_number))
+print(f"Division result of two numbers {first_number} and {second_number} is", divide(first_number,second_number))
