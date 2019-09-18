f5f49a710c8b53c136298756861f4c13babbcf90
sachinsuresh <sachinsuresh00@gmail.com>
2019-09-18 09:31:08 +0530

    Printed my details

---
+++ prints.py
@@ -0,0 +1,15 @@
+name = "Sachin"
+age = 23
+salary = 256843.56
+hair_colour = 'black'
+height = 185.1
+weight = 90.00
+education_degree = 'BE Mechanical Engineering'
+
+print("My name is", name)
+print("My age is", age)
+print("I have a monthly salary of Rs", salary)
+print("My hair is",hair_colour,"in colour")
+print("I am",height, "tall")
+print("I weigh",weight,"kgs")
+print("I have a",education_degree,"degree")
