2ce88c278e41683cb54b867684670b8cfc0063a2
sachinsuresh <sachinsuresh00@gmail.com>
2019-09-18 10:05:36 +0530

    Printed details seperated by commas in a line

---
+++ separators_1.py
@@ -0,0 +1,9 @@
+name = "Sachin"
+age = 23
+salary = 256843.56
+hair_colour = 'black'
+height = 185.1
+weight = 90.00
+education_degree = 'BE Mechanical Engineering'
+
+print(name,age,salary,hair_colour,height,weight,education_degree,sep=',')
